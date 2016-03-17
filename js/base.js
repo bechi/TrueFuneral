50c5065d7472a0370fed42cee99f9ed342605a04
bechi <bechilandia@gmail.com>
2016-03-17 09:19:26 -0300

    menu

--- js/base.js
+++ js/base.js
@@ -10,9 +10,7 @@ $(document).ready(function() {
       }
     });
   });
-});
 
-$(document).ready(function() {
   $(".dropdown-button").click(function() {
     var $button, $menu;
     $button = $(this);
@@ -23,10 +21,7 @@ $(document).ready(function() {
       $button.html($(this).html());
     });
   });
-});
 
-
-$(document).ready(function () {
   $('.accordion-tabs').each(function(index) {
     $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
   });
@@ -43,23 +38,136 @@ $(document).ready(function () {
       event.preventDefault();
     }
   });
-});
 
-$(function() {
-  $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
+  $(function() {
+    $("#modal-1").on("change", function() {
+      if ($(this).is(":checked")) {
+        $("body").addClass("modal-open");
+      } else {
+        $("body").removeClass("modal-open");
+      }
+    });
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+      e.stopPropagation();
+    });
+  });
+
+
+  ( function( window ) {
+
+  'use strict';
+
+  // class helper functions from bonzo https://github.com/ded/bonzo
+
+  function classReg( className ) {
+    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+  }
+
+  // classList support for class management
+  // altho to be fair, the api sucks because it won't accept multiple classes at once
+  var hasClass, addClass, removeClass;
+
+  if ( 'classList' in document.documentElement ) {
+    hasClass = function( elem, c ) {
+      return elem.classList.contains( c );
+    };
+    addClass = function( elem, c ) {
+      elem.classList.add( c );
+    };
+    removeClass = function( elem, c ) {
+      elem.classList.remove( c );
+    };
+  }
+  else {
+    hasClass = function( elem, c ) {
+      return classReg( c ).test( elem.className );
+    };
+    addClass = function( elem, c ) {
+      if ( !hasClass( elem, c ) ) {
+        elem.className = elem.className + ' ' + c;
+      }
+    };
+    removeClass = function( elem, c ) {
+      elem.className = elem.className.replace( classReg( c ), ' ' );
+    };
+  }
+
+  function toggleClass( elem, c ) {
+    var fn = hasClass( elem, c ) ? removeClass : addClass;
+    fn( elem, c );
+  }
+
+  var classie = {
+    // full names
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass,
+    toggleClass: toggleClass,
+    // short names
+    has: hasClass,
+    add: addClass,
+    remove: removeClass,
+    toggle: toggleClass
+  };
+
+  // transport
+  if ( typeof define === 'function' && define.amd ) {
+    // AMD
+    define( classie );
+  } else {
+    // browser global
+    window.classie = classie;
+  }
+
+  })( window );
+
+
+  (function() {
+    console.log("test");
+    var triggerBttn = document.getElementById( 'trigger-overlay' ),
+      overlay = document.querySelector( 'div.overlay' ),
+      closeBttn = overlay.querySelector( 'button.overlay-close' );
+      transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+      },
+      transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+      support = { transitions : Modernizr.csstransitions };
+
+    function toggleOverlay() {
+      if( classie.has( overlay, 'open' ) ) {
+        classie.remove( overlay, 'open' );
+        classie.add( overlay, 'close' );
+        var onEndTransitionFn = function( ev ) {
+          if( support.transitions ) {
+            if( ev.propertyName !== 'visibility' ) return;
+            this.removeEventListener( transEndEventName, onEndTransitionFn );
+          }
+          classie.remove( overlay, 'close' );
+        };
+        if( support.transitions ) {
+          overlay.addEventListener( transEndEventName, onEndTransitionFn );
+        }
+        else {
+          onEndTransitionFn();
+        }
+      }
+      else if( !classie.has( overlay, 'close' ) ) {
+        classie.add( overlay, 'open' );
+      }
     }
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    closeBttn.addEventListener( 'click', toggleOverlay );
   });
 
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
-
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
 });
 
