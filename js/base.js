657e9e8ff8b0572b9fd6f78281f3b4a15ec9a7d4
bechi <bechilandia@gmail.com>
2016-03-23 10:54:33 -0300

    empty state

--- js/base.js
+++ js/base.js
@@ -166,13 +166,41 @@ $(document).ready(function() {
 
     triggerBttn.addEventListener( 'click', toggleOverlay );
     closeBttn.addEventListener( 'click', toggleOverlay );
-
+44
 
    $('.step').waypoint(function(){
        $(this.element).toggleClass('animated');
     }, {
       offset: '95%'
     });
+
+
+  var fsvs = $.fn.fsvs({
+    speed : 5000,
+    bodyID : 'fsvs-body',
+    selector : '> .slide',
+    mouseSwipeDisance : 40,
+    afterSlide : function(){},
+    beforeSlide : function(){},
+    endSlide : function(){},
+    mouseWheelEvents : true,
+    mouseDragEvents : true,
+    touchEvents : true,
+    arrowKeyEvents : true,
+    pagination : true,
+    nthClasses : false,
+    detectHash : true
+  });
+
+  var slider = $.fn.fsvs({
+    endSlide : function(index) {
+      $('<div class="slide"><h2>Slide ' + (index+2) + '</h2></div>').appendTo( $('body') );
+      // reset the nth classes if you need it
+      slider.nthClasses(3);
+      // rebind pagination if you need it
+      slider.addPagination();
+    }
+  });
 
 });
 
