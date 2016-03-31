1c02fd569de5da08c096592bf6fafcf83c31f7c1
bechi <bechilandia@gmail.com>
2016-03-31 01:04:24 -0300

    about page

--- js/base.js
+++ js/base.js
@@ -175,32 +175,5 @@ $(document).ready(function() {
     });
 
 
-  var fsvs = $.fn.fsvs({
-    speed : 5000,
-    bodyID : 'fsvs-body',
-    selector : '> .slide',
-    mouseSwipeDisance : 40,
-    afterSlide : function(){},
-    beforeSlide : function(){},
-    endSlide : function(){},
-    mouseWheelEvents : true,
-    mouseDragEvents : true,
-    touchEvents : true,
-    arrowKeyEvents : true,
-    pagination : true,
-    nthClasses : false,
-    detectHash : true
-  });
-
-  var slider = $.fn.fsvs({
-    endSlide : function(index) {
-      $('<div class="slide"><h2>Slide ' + (index+2) + '</h2></div>').appendTo( $('body') );
-      // reset the nth classes if you need it
-      slider.nthClasses(3);
-      // rebind pagination if you need it
-      slider.addPagination();
-    }
-  });
-
 });
 
